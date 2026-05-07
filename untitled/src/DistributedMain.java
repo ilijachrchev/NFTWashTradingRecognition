828b0cb76933f62ed93f0c0af50cf9010d6fd1d6
ilijachrchev <ilijachrchev@gmail.com>
2026-05-07 21:37:13 +0200

    MPJ test

--- untitled/src/DistributedMain.java
+++ untitled/src/DistributedMain.java
@@ -1,7 +1,15 @@
+import mpi.MPI;
+
 public class DistributedMain {
 
     public static void main(String[] args) {
+        MPI.Init(args);
 
-        System.out.println("Hello World");
+        int rank = MPI.COMM_WORLD.Rank();
+        int size = MPI.COMM_WORLD.Size();
+
+        System.out.println("Hello from rank: " + rank + " of size " + size);
+
+        MPI.Finalize();
     }
 }
